e44fadaa76f2829b2b48b427e5d8f9214d66b734
Volodymyr <v_arish@ukr.net>
2023-11-30 17:22:07 +0200

    Введение в регулярные выражения в JavaScript

---
+++ jsSpREInr.js
@@ -0,0 +1,15 @@
+// №1
+/* 
+let str = 'ahb acb aeb aeeb adcb axeb ';
+let regExp = str.replace(/a.b/g, '!');
+
+// №2
+let str = 'aba aca aea abba adca abea';
+let pattern = /a..a/g;
+let newStr = str.replace(pattern, '!');
+*/
+// №3
+let text = 'aba aca aea abba adca abea';
+let regexPattern = /ab.a/g; 
+let alteredText = text.replace(regexPattern, '!');
+
