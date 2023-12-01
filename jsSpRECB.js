37ea6ae25492e2dc881ca1e640cf893cc73ba7d3
Volodymyr <v_arish@ukr.net>
2023-12-01 07:34:24 +0200

    Фигурные скобки в регулярных выражения JavaScript

---
+++ jsSpRECB.js
@@ -0,0 +1,6 @@
+// №1
+console.log('aa aba abba abbba abbbba abbbbba'.replace(/ab{2,4}a/g, '!'));
+// №2
+console.log('aa aba abba abbba abbbba abbbbba'.replace(/ab{1,3}a/g, '!'));
+// №3
+console.log('aa aba abba abbba abbbba abbbbba'.replace(/ab{4,}a/g, '!'));
